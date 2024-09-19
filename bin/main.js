3e111084ca887f9137badbb7e14eccdaf0a3ae09
voldo <filipecavoldo@outlook.com>
2024-09-19 19:41:23 -0300

    switched to sync operation

--- bin/main.js
+++ bin/main.js
@@ -6,12 +6,22 @@ const tasksData = __dirname + '/../tasksData.json';
 const command = process.argv[2];
 
 // Check if file exists before trying to use the program
-fs.readFile(tasksData, 'utf8', (err) => {
-  if (err) {
+try {
+  fs.readFileSync(tasksData, 'utf8')
+}
+catch(err) {
     createNewTasksData();
     return;
-  }
-});
+}
+
+//loading taskList from the taskData to further operations later
+try {
+  let taskList = JSON.parse(fs.readFileSync(tasksData, 'utf8'));
+}
+catch (err) {
+  console.error(`Couldn't open tasksData: ${err}`)
+}
+
 
 switch (command) {
   case 'add':
@@ -24,21 +34,22 @@ switch (command) {
 
 function addTask(){
   const taskName = process.argv[3];
-  console.log(taskName);
-}
 
+  console.log(taskName);
+
+}
 
 function createNewTasksData() {
   const content = {
-    "qtd": 0,
+    "qty": 0,
     "tasks": []
   };
 
-  fs.writeFile(tasksData, JSON.stringify(content), (err) => {
-    if(err) {
-      console.error(`can't create new tasks data file: ${err}`);
-      return;
-    }
-    console.log("Created new data file for your tasks");
-  });
+  try {
+    fs.writeFileSync(tasksData, JSON.stringify(content));
+  }
+  catch (err) {
+    console.error(`can't create new tasks data file: ${err}`);
+    return;
+  }
 }
